0a005d62ad9c440cfe34144cd806573807ea5863
Magnus Unger <magnus.unger@stendahls.se>
2015-04-14 15:38:09 +0200

    Added caching of fingerprinted urls

--- CacheBusting/FingerPrint.cs
+++ CacheBusting/FingerPrint.cs
@@ -1,6 +1,8 @@
 ﻿namespace CacheBusting
 {
     using System.IO.Abstractions;
+    using System.Web;
+    using System.Web.Caching;
 
     using CacheBusting.Abstractions;
     using CacheBusting.FingerPrinting;
@@ -9,18 +11,33 @@
     {
         public static string WithFileDate(string url, bool useQueryString = true)
         {
-            var generator = new FileDateFingerPrintGenerator(new FileSystem(), new PathMapper());
+            var pathMapper = new PathMapper();
+            var generator = new FileDateFingerPrintGenerator(new FileSystem(), pathMapper);
 
-            var fingerprinter = new FingerPrinter(generator, useQueryString);
-            return fingerprinter.FingerPrint(url);
+            return FingerPrintInternal(generator, pathMapper, url, useQueryString);
         }
 
         public static string WithContentHash(string url, bool useQueryString = true)
         {
-            var generator = new ContentHashFingerPrintGenerator(new FileSystem(), new PathMapper());
+            var pathMapper = new PathMapper();
+            var generator = new ContentHashFingerPrintGenerator(new FileSystem(), pathMapper);
 
-            var fingerprinter = new FingerPrinter(generator, useQueryString);
-            return fingerprinter.FingerPrint(url);
+            return FingerPrintInternal(generator, pathMapper, url, useQueryString);
+        }
+
+        private static string FingerPrintInternal(IFingerPrintGenerator fingerPrintGenerator, IPathMapper pathMapper, string virtualPath, bool useQueryString = true)
+        {
+            var cacheKey = string.Concat(virtualPath, "|", useQueryString, "|", fingerPrintGenerator.GetType().FullName);
+            if (HttpRuntime.Cache[cacheKey] == null)
+            {
+                var absolute = pathMapper.MapPath(virtualPath);
+                var fingerprinter = new FingerPrinter(fingerPrintGenerator, useQueryString);
+                var fingerprintedUrl = fingerprinter.FingerPrint(virtualPath);
+
+                HttpRuntime.Cache.Insert(cacheKey, fingerprintedUrl, new CacheDependency(absolute));
+            }
+
+            return HttpRuntime.Cache[cacheKey] as string;
         }
     }
 }
